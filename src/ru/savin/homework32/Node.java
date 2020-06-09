89f18b7fb454a121e65499127c21eb81e42f3a76
Savin Vladimir <8SavinVladimir8@gmail.com>
2020-06-09 15:09:23 +0500

    Homework 32 is ready.

---
+++ src/ru/savin/homework32/Node.java
@@ -0,0 +1,17 @@
+package ru.savin.homework32;
+
+/**
+ * Класс {@code Node} представляет собой узел бинарного дерева, хранящего целочисленные значения.
+ * @author Savin Vladimir
+ */
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+}
